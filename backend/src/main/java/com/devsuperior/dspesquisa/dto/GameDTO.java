26c037c78dc830de3d315bd114df4fa2c71a3eee
Michel Viuge <michelviuge@gmail.com>
2020-09-30 17:53:59 -0300

    DTO, Service

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/dto/GameDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dspesquisa.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+public class GameDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Platform platform;
+	
+	public GameDTO() {
+		
+	}
+
+	public GameDTO(Game entity) {
+		super();
+		this.id = entity.getId();
+		this.name = entity.getTitle();
+		this.platform = entity.getPlatform();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+	
+	
+	
+}
