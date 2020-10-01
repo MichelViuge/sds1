e210e5873f9ab6b46a4e19a6ac0d24f1936046b1
Michel Viuge <michelviuge@gmail.com>
2020-10-01 00:34:43 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,21 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+
+	@Query("select obj from Record obj WHERE "
+			+ "(coalesce(:minDate, null) is NULL OR obj.moment >= :minDate) AND "
+			+ "(coalesce(:maxDate, null) is NULL OR obj.moment <= :maxDate)")
+	Page<Record> findByMoments(Instant minDate, Instant maxDate, Pageable pageable);
 
 }
